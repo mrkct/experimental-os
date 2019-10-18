d5e4a14b34c2857d9ced6451f15092e86198129a
Marco Cutecchia <marco.cutecchia@studenti.unimi.it>
2019-10-18 22:43:57 +0200

    kprintf implementation for debugging

---
+++ kern/include/kernel/kprintf.h
@@ -0,0 +1,6 @@
+#ifndef KPRINTF_H
+#define KPRINTF_H
+
+int kprintf(char*, ...);
+
+#endif
---
+++ kern/kernel/kprintf.c
@@ -0,0 +1,54 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <kernel/tty.h>
+
+
+int kprintf(char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    int printed = 0;
+    char *current = format;
+    bool in_format = false;
+    while (*current) {
+        if (in_format) {
+            switch (*current) {
+            case 'd':
+                terminal_writeint(va_arg(args, int), 10);
+                break;
+            case '%':
+                terminal_putchar('%');
+                break;
+            case 'x':
+            case 'X':
+                terminal_writeint(va_arg(args, int), 16);
+                break;
+            case 'b':
+                terminal_writeint(va_arg(args, int), 2);
+                break;
+            case 's':
+                terminal_writestring(va_arg(args, char*));
+                break;
+            default:
+                terminal_writestring("[kprintf: unknown specifier]");
+                break;
+            }
+            in_format = false;
+            printed++;
+        } else {
+            if (*current == '%') {
+                in_format = true;
+            } else {
+                terminal_putchar(*current);
+            }
+        }
+        current++;
+    }
+
+    va_end(args);
+
+    return printed;
+}
