9f43b42effc54363da10ab6221df8222effa1392
Marco Cutecchia <marco.cutecchia@studenti.unimi.it>
2019-12-11 21:59:15 +0100

    RAMDISK: Working interface, can read & write bytes

--- src/kernel/devices/ramdisk/ramdisk.h
+++ src/kernel/devices/ramdisk/ramdisk.h
@@ -1,12 +1,11 @@
 #ifndef RAMDISK_H
 #define RAMDISK_H
 
-#define SECTOR_SIZE 512
+#include <kernel/devices/vdisk.h>
 
-#define ERR_RAMDISK_NOT_LOADED -1
-#define ERR_RAMDISK_SECTOR_OUT_OF_RANGE -2
-
-int read_sector(int sector, char *buffer);
-int write_sector(int sector, char *buffer);
+int ramdisk_init(char *disk, int size);
+int ramdisk_get_diskinterface(struct DiskInterface *interface);
+int read_bytes(int offset, int count, char *buffer);
+int write_bytes(int offset, int count, char *buffer);
 
 #endif
