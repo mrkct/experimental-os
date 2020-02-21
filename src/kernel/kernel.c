3a8f72ac5df52179b2969149329c3543f488580d
Marco Cutecchia <marco.cutecchia@studenti.unimi.it>
2020-02-21 19:00:54 +0100

    ATA: Removed testing code in kernel_main

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,23 +3,14 @@
 #include <stdint.h>
 #include <kernel/lib/kprintf.h>
 #include <kernel/lib/read_string.h>
+#include <kernel/lib/kassert.h>
 #include <kernel/monitor.h>
 #include <kernel/devices/ide/ide.h>
+#include <kernel/devices/vdisk.h>
 
 
 void kernel_main(void) 
 {
-    struct ide_identify_format id;
-    if (ide_identify_master(&id)) {
-        kprintf("ERROR identifying master IDE device\n");
-    } else {
-        kprintf(
-            "IDENTIFY Model: %s, capacity: %dMB\n", 
-            id.model, 
-            id.lba_capacity * 512 / 1024 / 1024
-        );
-    }
-
     #define BUFFER_SIZE 1024
     char buffer[BUFFER_SIZE];
     while (true) {
